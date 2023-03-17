9ccf35788f90c5b2af19345320cc6f30303f4cbc
FodorIustin <126153569+FodorIustin@users.noreply.github.com>
2023-03-17 11:22:53 +0200

    Smiley face iustin (#2)
    * add head
    
    * add eyes
    
    * eyess

--- scripts.js
+++ scripts.js
@@ -3,8 +3,29 @@ function setup() {
 }
 function draw(){
   background("#00ffff")
-
+  smileFaceIustin();
   smileFaceRadu();
+}
+
+function smileFaceIustin(){
+  //skin
+  fill(255, 204, 0)
+  circle(200,200, 100)
+
+  //eyes
+  circle(217,190,10)
+  circle(217,190,1)
+
+  circle(183,190,10)
+  circle(183,190,1)
+
+  line(170, 170, 190, 190);
+  line(230, 200, 210, 210);
+
+  //mouth
+  arc(200 ,220 , 43 , 25 , 0 ,Math.PI)
+
+
 }
 
 
