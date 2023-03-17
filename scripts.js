f5d42a258d0b3fc0076864457b3328190932064c
Radu Crisan <radu.crisan@ekkode.com>
2023-03-17 11:14:38 +0200

    add hair

--- scripts.js
+++ scripts.js
@@ -13,9 +13,26 @@ function smileFaceRadu() {
   fill("#ffff00");
   circle(200, 200, 100);
 
-  // right eye
+  // hair 
+  fill("#000000");
+  line(170, 150, 180, 160)
+  line(175, 140, 180, 160)
+  line(190, 140, 190, 160)
+  line(205, 140, 190, 160)
+  line(215, 140, 200, 160)
+  line(225, 140, 200, 160)
+
+  // eyes
+  fill("#ffffff");
+  circle(180, 180, 20);
   circle(220, 180, 20);
 
-  // left eye
-  circle(180, 180, 20);
+  // iris
+  fill("#0000ff");
+  circle(220, 180, 7) 
+  circle(180, 180, 7)
+
+  // mouth
+  fill("#ff0000");
+  arc(200, 220, 40, 20, 0, Math.PI)
 }
