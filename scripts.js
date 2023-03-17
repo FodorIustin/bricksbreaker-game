b2070c5e978c6de37a705e4f359d696b31fc7778
FodorIustin <iustin.fodor@emanuel.ro>
2023-03-17 11:16:02 +0200

    eyess

--- scripts.js
+++ scripts.js
@@ -10,7 +10,19 @@ function smileFaceIustin(){
   //skin
   fill(255, 204, 0)
   circle(200,200, 100)
+
   //eyes
   circle(217,190,10)
+  circle(217,190,1)
+
   circle(183,190,10)
+  circle(183,190,1)
+
+  line(170, 170, 190, 190);
+  line(230, 200, 210, 210);
+
+  //mouth
+  arc(200 ,220 , 43 , 25 , 0 ,Math.PI)
+
+
 }
